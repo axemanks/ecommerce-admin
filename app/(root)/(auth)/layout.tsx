b2396be0a0d5a9df75cac4dde1f405b0ee2f52c1
KeithScheldt <keithscheldt@gmail.com>
2023-11-11 16:05:28 -0500

    added clerk auth

---
+++ app/(root)/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+// layout for the sign in and sign-up pages
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex items-center justify-center h-full">
+        {children}
+    </div>
+  );
+}
